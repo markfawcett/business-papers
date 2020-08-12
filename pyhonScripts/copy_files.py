e92c93204784d3c72900b99f8fec15fac7a2876a
Mark Fawcett <markjfawcett@gmail.com>
2020-08-12 15:59:37 +0100

    Clean up

--- pyhonScripts/copy_files.py
+++ pyhonScripts/copy_files.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 
 def main():
+
     # where do we want to copy files to
     existing_html_Path = Path(r'C:\Users\fawcettm\projects\business-papers\existingHTML')
     # i gress find a bunch of files that follow a pattern
@@ -15,7 +16,6 @@ def main():
         print(f'copying: {str(file_Path)} to {str(copy_to_Path)}')
 
         copyfile(file_Path, copy_to_Path)
-
 
 if __name__ == '__main__':
     main()
